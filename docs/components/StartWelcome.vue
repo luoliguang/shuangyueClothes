4b33440bd20f0317fc305157ec607ebdc50534bb
luoliguang <2298928961@qq.com>
2024-11-28 13:34:49 +0800

    更新主页轮播图样式

--- docs/components/StartWelcome.vue
+++ docs/components/StartWelcome.vue
@@ -1,20 +1,4 @@
 <template>
-  <!-- <div class="scroll-container" ref="scrollContainer">
-    <div class="scroll-content" ref="scrollContent">
-      <div class="swiper-slide">Slide 1</div>
-      <div class="swiper-slide">Slide 2</div>
-      <div class="swiper-slide">Slide 3</div>
-      <div class="swiper-slide">Slide 4</div>
-      <div class="swiper-slide">Slide 5</div>
-      <div class="swiper-slide">Slide 6</div>
-      <div class="swiper-slide">Slide 7</div>
-      <div class="swiper-slide">Slide 8</div>
-      <div class="swiper-slide">Slide 9</div>
-      <div class="swiper-slide">Slide 10</div>
-      <div class="swiper-slide">Slide 11</div>
-      <div class="swiper-slide">Slide 12</div>
-    </div>
-  </div> -->
   <div class="scroll-container" ref="scrollContainer">
     <div class="scroll-content" ref="scrollContent">
       <img src="/else/首页素材/1.jpg" alt="" class="swiper-slide">
