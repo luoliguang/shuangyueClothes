96f134f019d1d3ab1355a881ec428b4c9496f96b
luoliguang <2298928961@qq.com>
2024-12-09 14:31:01 +0800

    新增VueLazyload配置文件 懒加载

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -4,7 +4,8 @@ import Theme from "vitepress/theme"
 import "./vars.css";
 // import "./styles/style.scss";
 import "vitepress-markdown-timeline/dist/theme/index.css"; // 时间线样式
-import VueLazyload from 'vue-lazyload'; // 懒加载
+// import VueLazyload from 'vue-lazyload'; // 懒加载
+import { setupLazyload } from '../../components/data/VueLazyload'
 
 export default {
   ...DefaultTheme,
@@ -15,19 +16,21 @@ export default {
       Theme.enhanceApp({ app, router, siteData });
     }
 
-    // 配置懒加载
-    app.use(VueLazyload, {
-      loading: 1.3,
-      error: '/loading/error.gif', //加载失败
-      loadingImage: '/loading/loading.gif', //加载中
-      attempt: 1,
-      //观察者选项
-      observer: true,
-      observerOptions: {
-        rootMargin: '0px',
-        threshold: 0.1,
-      },
-    });
+    // // 配置懒加载
+    // app.use(VueLazyload, {
+    //   loading: 1.3,
+    //   error: '/loading/error.gif', //加载失败
+    //   loadingImage: '/loading/loading.gif', //加载中
+    //   attempt: 1,
+    //   //观察者选项
+    //   observer: true,
+    //   observerOptions: {
+    //     rootMargin: '0px',
+    //     threshold: 0.1,
+    //   },
+    // });
+    // 设置懒加载
+    setupLazyload(app)
 
     // 全局注册组件
     Object.entries(components).forEach(([name, component]) => {
