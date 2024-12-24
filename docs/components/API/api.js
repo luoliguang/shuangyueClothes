c0912a0b62142bb7857c871075a9cf4778b5e266
luoliguang <2298928961@qq.com>
2024-12-24 09:06:53 +0800

    新增api文件

--- docs/components/API/api.js
+++ docs/components/API/api.js
@@ -1,18 +1,21 @@
 // src/api.js
 import axios from 'axios';
 
-const apiUrl = 'https://7bu.top/api/v1/images';
-const token = '1607|NhSDNdWduqwRvuIQDOvey1vsaTCQL93BFei0J6jc'; // 替换成你获取到的Token
+const baseUrl = 'https://7bu.top/api/v1/images';
+const token = '1607|NhSDNdWduqwRvuIQDOvey1vsaTCQL93BFei0J6jc';
 
-const getImages = async () => {
+const getImages = async (albumId) => {
   try {
-    const response = await axios.get(apiUrl, {
+    const response = await axios.get(`${baseUrl}`, {
+      params: {
+        album_id: albumId
+      },
       headers: {
         'Authorization': `Bearer ${token}`,
         'Accept': 'application/json',
       }
     });
-    console.log('图床返回的图片数据:', response.data);
+    // console.log('图床返回的图片数据:', response.data);
     return response.data;
   } catch (error) {
     console.error('获取图片列表失败:', error);
